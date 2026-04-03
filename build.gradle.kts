9f3931529ebf8884c2061101a82327bf03f5f95d
BigJins <sp04212016@gmail.com>
2026-04-04 00:16:31 +0900

    fix: Spring Boot 4.0 OTLP 트레이싱 AutoConfig 모듈 추가 (spring-boot-micrometer-tracing)

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,6 +36,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     runtimeOnly("io.micrometer:micrometer-registry-prometheus")
+    implementation("org.springframework.boot:spring-boot-micrometer-tracing")
     implementation("io.micrometer:micrometer-tracing-bridge-otel")
     implementation("io.opentelemetry:opentelemetry-exporter-otlp")
     implementation("com.github.loki4j:loki-logback-appender:1.5.2")
